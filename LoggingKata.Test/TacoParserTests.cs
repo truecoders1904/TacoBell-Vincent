a20f9a7b96fb7eeb65a5b3d59ee752de113d6523
Vincent <vincenttafolla1@gmail.com>
2019-05-20 10:26:26 -0500

    finished project

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -5,25 +5,44 @@ namespace LoggingKata.Test
 {
     public class TacoParserTests
     {
-        [Fact]
-        public void ShouldDoSomething()
-        {
-            // TODO: Complete Something, if anything
-        }
 
         [Theory]
-        [InlineData("Example")]
+        [InlineData("0,0,Tacobell")]
+        [InlineData("43.43,32.32,Tacobell")]
         public void ShouldParse(string str)
         {
-            // TODO: Complete Should Parse
+            // Arange
+            TacoParser tacoParser = new TacoParser();
+
+            // Act
+            ITrackable actual = tacoParser.Parse(str);
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.NotNull(actual.Location);
+            Assert.NotNull(actual.Name);
+            Assert.True(actual.Name.Length > 0);
         }
 
         [Theory]
         [InlineData(null)]
         [InlineData("")]
+        [InlineData("0,abc,TacoBell")]
+        [InlineData("abc,0,TacoBell")]
+        [InlineData("0,0")]
+        [InlineData("300,0,TacoBell")]
+        [InlineData("0,300,TacoBell")]
+        [InlineData("abc,0,TacoBell")]
         public void ShouldFailParse(string str)
         {
-            // TODO: Complete Should Fail Parse
+            // Arrange
+            TacoParser tacoParser = new TacoParser();
+
+            // Act
+            ITrackable actual = tacoParser.Parse(str);
+
+            // Assert
+            Assert.Null(actual);
         }
     }
 }
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -9,10 +9,49 @@
         
         public ITrackable Parse(string line)
         {
-            logger.LogInfo("Begin parsing");
 
-            // Do not fail if one record parsing fails, return null
-            return null; // TODO Implement
+            if (line == null)
+            {
+                return null;
+            }
+
+            var cells = line.Split(',');
+
+            if (cells.Length < 3)
+            {
+                logger.LogError("Not enough info on Line.");
+                return null;
+            }
+
+            double lat;
+            double lon;
+            try
+            {
+                lat = double.Parse(cells[0]);
+                lon = double.Parse(cells[1]);
+            }
+            catch
+            {
+                return null;
+            }
+            if (lat > 90 || lat < -90)
+            {
+                return null;
+            }
+            if (lon > 180 || lon < -180)
+            {
+                return null;
+            }
+            TacoBell tacobell = new TacoBell();
+            tacobell.Name = cells[2];
+
+            Point point = new Point();
+            point.Latitude = lat;
+            point.Longitude = lon;
+
+            tacobell.Location = point;
+
+            return tacobell;
         }
     }
 }
